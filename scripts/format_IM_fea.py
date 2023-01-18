3b0ab45e5368a686412e5336b030154813f794f6
Josh Meyers <jmeyers314@gmail.com>
2023-01-18 14:15:28 -0800

    Add default IM dir

--- scripts/format_IM_fea.py
+++ scripts/format_IM_fea.py
@@ -6,24 +6,25 @@ from scipy.interpolate import Rbf
 
 
 def main(args):
+    os.makedirs(args.outdir, exist_ok=True)
     # M1M3_1um_156_grid, force
     # Txt files are more recent, so using those
-    # arr1 = np.load(os.path.join(args.indir, "data", "M1M3", "M1M3_1um_156_grid.npy"))  # Is not PTT-subtracted!
-    arr = np.loadtxt(os.path.join(args.indir, "data", "M1M3", "M1M3_1um_156_grid.txt"))  # Is PTT-subtracted!
+    # arr1 = np.load(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_1um_156_grid.npy"))  # Is not PTT-subtracted!
+    arr = np.loadtxt(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_1um_156_grid.txt"))  # Is PTT-subtracted!
     m13x = arr[:, 1]
     m13y = arr[:, 2]
     fits.writeto(os.path.join(args.outdir, "M1M3_1um_156_grid.fits.gz"), arr, overwrite=True)
-    arr = np.loadtxt(os.path.join(args.indir, "data", "M1M3", "M1M3_1um_156_force.txt"))
+    arr = np.loadtxt(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_1um_156_force.txt"))
     fits.writeto(os.path.join(args.outdir, "M1M3_1um_156_force.fits.gz"), arr, overwrite=True)
 
     # M1M3_dxdydz_zenith, horizon
-    arr = np.load(os.path.join(args.indir, "data", "M1M3", "M1M3_dxdydz_zenith.npy"))
+    arr = np.load(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_dxdydz_zenith.npy"))
     fits.writeto(os.path.join(args.outdir, "M1M3_dxdydz_zenith.fits.gz"), arr, overwrite=True)
-    arr = np.load(os.path.join(args.indir, "data", "M1M3", "M1M3_dxdydz_horizon.npy"))
+    arr = np.load(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_dxdydz_horizon.npy"))
     fits.writeto(os.path.join(args.outdir, "M1M3_dxdydz_horizon.fits.gz"), arr, overwrite=True)
 
     # M1M3_thermal_FEA
-    arr = np.load(os.path.join(args.indir, "data", "M1M3", "M1M3_thermal_FEA.npy"))
+    arr = np.load(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_thermal_FEA.npy"))
     # One time interpolation here
     tx = arr[:, 0]  # radius normalized in M1M3 CS
     ty = arr[:, 1]
@@ -39,34 +40,34 @@ def main(args):
     )
 
     # M1M3_force_zenith, horizon
-    arr = np.load(os.path.join(args.indir, "data", "M1M3", "M1M3_force_zenith.npy"))
+    arr = np.load(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_force_zenith.npy"))
     fits.writeto(os.path.join(args.outdir, "M1M3_force_zenith.fits.gz"), arr, overwrite=True)
-    arr = np.load(os.path.join(args.indir, "data", "M1M3", "M1M3_force_horizon.npy"))
+    arr = np.load(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_force_horizon.npy"))
     fits.writeto(os.path.join(args.outdir, "M1M3_force_horizon.fits.gz"), arr, overwrite=True)
 
     # M1M3_influence_256
-    arr = np.load(os.path.join(args.indir, "data", "M1M3", "M1M3_influence_256.npy"))
+    arr = np.load(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_influence_256.npy"))
     fits.writeto(os.path.join(args.outdir, "M1M3_influence_256.fits.gz"), arr, overwrite=True)
 
     # M1M3_LUT
-    arr = np.loadtxt(os.path.join(args.indir, "data", "M1M3", "M1M3_LUT.txt"))
+    arr = np.loadtxt(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_LUT.txt"))
     fits.writeto(os.path.join(args.outdir, "M1M3_LUT.fits.gz"), arr, overwrite=True)
 
     # M1M3_1000N_UL_shape_156
-    arr = np.load(os.path.join(args.indir, "data", "M1M3", "M1M3_1000N_UL_shape_156.npy"))
+    arr = np.load(os.path.join(args.IM_dir, "data", "M1M3", "M1M3_1000N_UL_shape_156.npy"))
     fits.writeto(os.path.join(args.outdir, "M1M3_1000N_UL_shape_156.fits.gz"), arr, overwrite=True)
 
     # M2_1um_grid, force
-    arr = np.loadtxt(os.path.join(args.indir, "data", "M2", "M2_1um_grid.DAT"))  # Is PTT-subtracted!
+    arr = np.loadtxt(os.path.join(args.IM_dir, "data", "M2", "M2_1um_grid.DAT"))  # Is PTT-subtracted!
     # Save these for below
     m2x = arr[:, 1]  # meters
     m2y = arr[:, 2]
     fits.writeto(os.path.join(args.outdir, "M2_1um_grid.fits.gz"), arr, overwrite=True)
-    arr = np.loadtxt(os.path.join(args.indir, "data", "M2", "M2_1um_force.DAT"))  # Is PTT-subtracted!
+    arr = np.loadtxt(os.path.join(args.IM_dir, "data", "M2", "M2_1um_force.DAT"))  # Is PTT-subtracted!
     fits.writeto(os.path.join(args.outdir, "M2_1um_force.fits.gz"), arr, overwrite=True)
 
     # One time interpolation here
-    arr = np.loadtxt(os.path.join(args.indir, "data", "M2", "M2_GT_FEA.txt"), skiprows=1)  # Is PTT-subtracted!
+    arr = np.loadtxt(os.path.join(args.IM_dir, "data", "M2", "M2_GT_FEA.txt"), skiprows=1)  # Is PTT-subtracted!
     tx = arr[:, 0]  # Radius-normalized coordinates in M2 CS
     ty = arr[:, 1]
     zdz = Rbf(tx, ty, arr[:, 2])(m2x/1.71, m2y/1.71)
@@ -84,7 +85,7 @@ def main(args):
         "FPRB.txt", "L1RB.txt", "L1S2zer.txt", "L2S1zer.txt", "L3RB.txt", "L3S2zer.txt",
         "FRB.txt", "L1S1zer.txt", "L2RB.txt", "L2S2zer.txt", "L3S1zer.txt"
     ]:
-        arr = np.loadtxt(os.path.join(args.indir, "data", "camera", fn), skiprows=1)
+        arr = np.loadtxt(os.path.join(args.IM_dir, "data", "camera", fn), skiprows=1)
         fits.writeto(os.path.join(args.outdir, fn.replace(".txt", ".fits.gz")), arr, overwrite=True)
 
 
@@ -92,8 +93,10 @@ if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument(
-        "indir",
+        "--IM_dir",
+        help="Path to the ZEMAX_FEMAP directory",
         type=str,
+        default="/Users/josh/src/IM/"
     )
     parser.add_argument(
         "--outdir",
