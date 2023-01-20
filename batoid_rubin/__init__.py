2f04efc4f37bc35faca9df7f040edd70c1734e6d
Josh Meyers <jmeyers314@gmail.com>
2023-01-20 13:57:21 -0800

    Add version

--- batoid_rubin/__init__.py
+++ batoid_rubin/__init__.py
@@ -1,3 +1,9 @@
+__version__ = "0.1.0"
+tmp = __version__
+if "rc" in tmp:  # pragma: no cover
+    tmp = tmp[:tmp.find("rc")]
+__version_info__ = tuple(map(int, tmp.split('.')))
+
 from .builder import *
 from .visualize import *
 from .align_game import AlignGame
