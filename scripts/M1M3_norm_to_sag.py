238e4921a2b3c63c299decc7160e1de050189832
Josh Meyers <jmeyers314@gmail.com>
2023-09-14 11:31:39 -0700

    Use actuator tables in NASTRAN scripts

--- scripts/M1M3_norm_to_sag.py
+++ scripts/M1M3_norm_to_sag.py
@@ -6,7 +6,7 @@ import batoid
 def main(args):
     telescope = batoid.Optic.fromYaml("LSST_r.yaml")
     with open(args.input, 'rb') as f:
-        x, y, w1, w3, Udn3norm, Vdn3norm = pickle.load(f)
+        x, y, w1, w3, Udn3norm, Vdn3norm, coef = pickle.load(f)
     Udn3norm[w1] /= telescope['M1'].surface.normal(x[w1], y[w1])[:, 2][:, None]
     Udn3norm[w3] /= telescope['M3'].surface.normal(x[w3], y[w3])[:, 2][:, None]
     with open(args.output, 'wb') as f:
--- scripts/M2_norm_to_sag.py
+++ scripts/M2_norm_to_sag.py
@@ -6,7 +6,7 @@ import batoid
 def main(args):
     telescope = batoid.Optic.fromYaml("LSST_r.yaml")
     with open(args.input, 'rb') as f:
-        x, y, Udn3norm, Vdn3norm = pickle.load(f)
+        x, y, Udn3norm, Vdn3norm, coef = pickle.load(f)
     Udn3norm /= telescope['M2'].surface.normal(x, y)[:, 2][:, None]
     with open(args.output, 'wb') as f:
         pickle.dump((x, y, Udn3norm, Vdn3norm), f)
