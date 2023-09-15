bb2668c295031cad50f118ed2bf745f3a70b26cc
Josh Meyers <jmeyers314@gmail.com>
2023-09-14 17:11:56 -0700

    Use asdf in M2 scripts

--- scripts/M2_NASTRAN.py
+++ scripts/M2_NASTRAN.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-import pickle
 
 import batoid
 from galsim.zernike import Zernike, zernikeBasis
 import numpy as np
 from scipy.io import loadmat
 from astropy.table import Table
+import asdf
 
 
 M1_outer = 4.18
@@ -29,6 +29,10 @@ def main(args):
     actuators['x'] *= 0.0254  # inches -> meters
     actuators['y'] *= 0.0254
 
+    tree = dict()
+    tree['actuators'] = actuators
+    nactuators = len(actuators)
+
     # Load NASTRAN perturbations
     t1t2t3 = loadmat(indir / "0unitLoadCases" / "T1T2T3.mat")['NXYZap']
     nodeID = t1t2t3[:, 0]
@@ -43,6 +47,15 @@ def main(args):
     fea_modes = np.array([dx, dy, dz]).T
     nmode, nnode, _ = fea_modes.shape
 
+    fea_nodes = Table()
+    fea_nodes['nodeID'] = nodeID
+    fea_nodes['X_Position'] = x
+    fea_nodes['Y_Position'] = y
+    fea_nodes['Z_Position'] = z
+    tree['fea_nodes'] = fea_nodes
+    tree['unit_load'] = dict()
+    tree['unit_load']['displacements'] = fea_modes
+
     # Load telescope so we can project perturbations onto surface normal
     telescope = batoid.Optic.fromYaml("LSST_r.yaml")
     normal_vectors = np.empty((nnode, 3))
@@ -50,55 +63,83 @@ def main(args):
     # Do the projection
     normal_modes = np.einsum("abc,bc->ab", fea_modes, normal_vectors)
 
+    ptt_modes = np.array(normal_modes)
+
+    tree['M2_normal_vectors'] = normal_vectors
+    tree['unit_load']['normal_displacement'] = normal_modes
+
     # Optionally subtract PTT modes
     if args.M2ptt > 0:
         zbasis = zernikeBasis(
             args.M2ptt, x, y, R_inner=M2_inner, R_outer=M2_outer
         )
         for imode in range(nmode):
-            coefs, *_ = np.linalg.lstsq(zbasis.T, normal_modes[imode], rcond=None)
-            normal_modes[imode] -= Zernike(
+            coefs, *_ = np.linalg.lstsq(zbasis.T, ptt_modes[imode], rcond=None)
+            ptt_modes[imode] -= Zernike(
                 coefs[:4], R_inner=M2_inner, R_outer=M2_outer
             )(x, y)
+
+    tree['unit_load']['ptt_normal_displacement'] = ptt_modes
 
     # Load balanced unit load cases and form pseudo-inverse
     C = np.full((nmode, nmode), np.nan)
     for i, actuator in enumerate(actuators):
         file = indir/"0unitLoadCases"/f"node{actuator['id']}.csv"
         C[i] = np.genfromtxt(file, delimiter=',')[:, 1]
+
+    tree['unit_load']['forces'] = C
+
     s = np.linalg.svd(C)[1]
     Cinv = np.linalg.pinv(C, rcond=s[0]*1e-6)
 
+    influence = Cinv@ptt_modes
+    tree['influence_normal_displacement'] = influence
+
     # Solve for FEA modes
-    u, s, vh = np.linalg.svd(Cinv@normal_modes, full_matrices=False)
+    u, s, vh = np.linalg.svd(influence, full_matrices=False)
 
     # Normalize to 1um surface normal deviation
+    normal_modes_1um = np.empty((nmode, nnode))
+    force_modes_1um = np.empty((nmode, nactuators))
+    coef_1um = np.empty((nmode, nmode))  # coefs of unit load force or deviation vectors
+
+
     Udn3norm = np.empty((nnode, nmode))  # surface deviations
     Vdn3norm = np.empty((nmode, nmode))  # actuator forces
     coef = np.empty((nmode, nmode))  # coefs of unit load force or deviation vectors
     for imode in range(nmode):
         factor = 1e-6/np.std(vh[imode])
-        Udn3norm[:, imode] = vh[imode] * factor
-        Vdn3norm[:, imode] = u[:, imode] * factor / s[imode]
-        coef[:, imode] = Cinv@Vdn3norm[:, imode]
+        normal_modes_1um[imode] = vh[imode] * factor
+        force_modes_1um[imode] = u.T[imode] * factor / s[imode]
+        # coef_1um[imode], *_ = np.linalg.lstsq(ptt_modes.T, normal_modes_1um[imode], rcond=None)
+        coef_1um[imode] = Cinv@force_modes_1um[imode]
 
         if imode < 30:  # mostly care about first ~30 modes
             np.testing.assert_allclose(
-                coef[:, imode]@normal_modes, Udn3norm[:, imode],
+                coef_1um[imode]@ptt_modes, normal_modes_1um[imode],
                 atol=1e-9, rtol=0.0
             )
 
             np.testing.assert_allclose(
-                coef[:, imode]@C, Vdn3norm[:, imode],
+                coef_1um[imode]@C, force_modes_1um[imode],
                 atol=1e-3, rtol=1e-3
             )
 
-    Udn3sag = np.array(Udn3norm)
-    Udn3sag /= telescope['M2'].surface.normal(x, y)[:, 2][:, None]
+    tree['bend_1um'] = dict()
+    tree['bend_1um']['normal'] = normal_modes_1um
+    tree['bend_1um']['force'] = force_modes_1um
+    tree['bend_1um']['coef'] = coef_1um
+
+    # Convert displacement along normal to along sag
+    sag_modes_1um = np.array(normal_modes_1um)
+    sag_modes_1um /= telescope['M2'].surface.normal(x, y)[:, 2]
+
+    tree['bend_1um']['sag'] = sag_modes_1um
+    tree['args'] = vars(args)
 
     # Note: there are arbitrary minus signs here we don't track.
-    with open(args.output, 'wb') as f:
-        pickle.dump((x, y, Udn3norm, Vdn3norm, Udn3sag, coef), f)
+    # We might also have some mode ordering swaps when switching PTT off and on.
+    asdf.AsdfFile(tree).write_to(args.output)
 
 
 if __name__ == "__main__":
@@ -123,10 +164,10 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "output",
-        default="M2_norm.pkl",
+        default="M2_norm.asdf",
         help=
             "output file name.  "
-            "Default: M2_norm.pkl",
+            "Default: M2_norm.asdf",
         nargs='?'
     )
     args = parser.parse_args()
--- scripts/improved.sh
+++ scripts/improved.sh
@@ -11,6 +11,6 @@ python M1M3_decompose_sag.py --input $OUT_DIR/M1M3_improved.asdf $OUT_DIR/M1M3_d
 python M1M3_format_for_batoid.py --input $OUT_DIR/M1M3_decomposition_improved.asdf --outdir $OUT_DIR --nkeep 30
 
 
-python M2_NASTRAN.py --M2ptt 6 --indir $NASTRAN_DIR $OUT_DIR/M2_improved.pkl
-python M2_decompose_sag.py --input $OUT_DIR/M2_improved.pkl $OUT_DIR/M2_decomposition_improved.pkl
-python M2_format_for_batoid.py --input $OUT_DIR/M2_decomposition_improved.pkl --outdir $OUT_DIR --nkeep 30
+python M2_NASTRAN.py --M2ptt 6 --indir $NASTRAN_DIR $OUT_DIR/M2_improved.asdf
+python M2_decompose_sag.py --input $OUT_DIR/M2_improved.asdf $OUT_DIR/M2_decomposition_improved.asdf
+python M2_format_for_batoid.py --input $OUT_DIR/M2_decomposition_improved.asdf --outdir $OUT_DIR --nkeep 30
